b9679bcb1f49fd92d478af108bc1c6505b38acf3
Tim Kellogg <timmkellogg@gmail.com>
2022-03-14 17:36:42 -0400

    remember to include server file!

---
+++ module13/session2/server.js
@@ -0,0 +1,24 @@
+import express from 'express';
+import path from 'path';
+
+import authenticateJWT from './middleware/authenticateJWT.js';
+
+import authRouter from './routes/auth.js';
+import gifRouter from './routes/gifs.js';
+
+const app = express();
+const port = process.env.PORT || 3001;
+
+app.use(express.urlencoded({ extended: false }))
+
+
+app.use('/auth', authRouter);
+app.use('/gifs', authenticateJWT, gifRouter);
+
+app.get('*', (req, res) => {
+    res.sendFile(path.join(path.resolve(), 'index.html'));
+});
+
+app.listen(port, () => {
+    console.log(`app listening on port ${port}`)
+})
